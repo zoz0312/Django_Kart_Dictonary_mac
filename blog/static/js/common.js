6554da0907afccacad47491a1a674892adfe3320
AJu <zoz0312@naver.com>
2018-01-31 21:05:59 +0900

    add tab animation

--- blog/static/js/common.js
+++ blog/static/js/common.js
@@ -1,21 +1,30 @@
 $(document).ready(function(){
+    tab_css('tab1');
+
     for( var i=1; i<=3; i++ ){
         var tab = ".tab"+i;
         $(tab).click(function(){
-            for( var j=1; j<=3; j++ ){
-                var change_tab = ".tab"+j;
-                if( "tab"+j != $(this).attr('class') ){
-                    $(change_tab).css({
-                      'background-color': '#999999'
-                    });
-                } else {
-                    $(change_tab).css({
-                      'background-color': '#428bca'
-                    });
-                }
-            }
-            console.log($(this).attr('class'));
+            tab_css($(this).attr('class'));
         });
+    }
+
+    function tab_css( name ){
+        for( var j=1; j<=3; j++ ){
+            var change_tab = ".tab"+j;
+            if( "tab"+j != name ){
+                $(change_tab).css({
+                    'backgroundColor':'#999999',
+                    'height':'70%',
+                    'line-height': '60px'
+                });
+            } else {
+                $(change_tab).css({
+                  'backgroundColor': '#428bca',
+                  'height':'100%',
+                  'line-height': '84px'
+                }, 1000);
+            }
+        }
     }
 });
 
